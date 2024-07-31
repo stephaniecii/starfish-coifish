2eae98767623683e0b17a0de0ed7173fbde2768c
stephaniecii <142693906+stephaniecii@users.noreply.github.com>
2024-07-30 20:55:37 -0700

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,15 @@
+//terms array
+const terms = ["Terms of Service", "TOS", "Privacy Policy", "Terms and Conditions"];
 
+// Function to check if any of the terms appear in the text content of the document
+function containsTerms(text, terms) {
+  return terms.some(term => text.includes(term));
+}
+
+// Get the text content of the entire document
+const textContent = document.body.textContent || document.body.innerText;
+
+if (containsTerms(textContent, terms)) {
+  console.log("Terms detected!");
+  //add acttions here
+}
