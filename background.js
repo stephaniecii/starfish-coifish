ecbf0663d797545bc906cdacb4a2f7bff0997abe
cindylll022 <cindyllllll022@gmail.com>
2024-08-08 08:01:41 -0700

    send message & open popup

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
+  if (changeInfo.status === 'complete'&& tab.active) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
       files: ['scripts/content.js']
     });
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
+  if (message.termsDetected){
+    chrome.action.openPopup();
   }
 });
